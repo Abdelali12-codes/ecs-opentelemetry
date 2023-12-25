256a9eecf81764b5bbe36ed0c97f87ca3c654bb3
Abdelali12-codes <jadelmoulaa3@gmail.com>
2023-12-25 15:43:29 +0000

    update code

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -213,7 +213,7 @@ class CdkStack(Stack):
                   )
             )
         
-        nginxcontainer.add_container_dependencies(flaskcontainer)
+        nginxcontainer.add_container_dependencies(ecs.ContainerDependency(container=flaskcontainer))
         
         # Flask application security group
         
